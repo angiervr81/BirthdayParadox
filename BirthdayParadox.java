20c8e718b94d25abe9a09feeb6b295debf10260f
Angie <Angie.Rivera81@bcmail.cuny.edu>
2024-11-16 15:57:06 -0500

    Creating code
    Here I will add all of my code using VS

---
+++ BirthdayParadox.java
@@ -0,0 +1,20 @@
+
+/* Write a piece of code that finds out at least 
+how many individuals in a group are needed so that 
+the probability of two people's sharing the 
+same birthday is more than 69%.*/
+
+public class BirthdayParadox{
+    /* This method will calculate the number of people 
+     * needed for a probability of at least 2 people sharing 
+     * the same birthday
+     */
+
+     public static int calPeople (double probability){
+        int numOfpeople= 0; // we want to start at 0 people 
+        double probabilityNotShare=1.0; // we want to people that don't share Bdays
+
+        //we want to keep adding people to the number of
+     }
+    
+}
