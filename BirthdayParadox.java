6f718b6fcc9cf758234de5a9315230f832d823d3
Angie <Angie.Rivera81@bcmail.cuny.edu>
2024-11-16 20:00:15 -0500

    Finshed while loop and add a main

--- BirthdayParadox.java
+++ BirthdayParadox.java
@@ -10,11 +10,38 @@ public class BirthdayParadox{
      * the same birthday
      */
 
-     public static int calPeople (double probability){
+     public static int calPeople (double tarProbability){
         int numOfpeople= 0; // we want to start at 0 people 
         double probabilityNotShare=1.0; // we want to people that don't share Bdays
 
-        //we want to keep adding people to the number of
+        // Creating a while loop to get the probability of at least 2 people
+        // sharing a birthfay that is less than the target probability so lets 
+        // keep adding people
+        while(true){
+            numOfpeople++; 
+            probabilityNotShare *=(365 - (numOfpeople-1))/365.0; //updating probability
+
+            // calculating the probability that at least 2 share a birthday
+            double sharedProbability = 1- probabilityNotShare;
+
+            //Check if we have reached the target or passed it
+            if(sharedProbability >= tarProbability){
+                break; // if target is reached exit loop
+            }
+        }
+        return numOfpeople; //return the total num of people needed
      }
-    
+
+
+     // main method 
+     public static void main(String[] args){
+        double requiredProbability = 0.69; // set the required probability to 69%
+
+        //call the method calPeople to calculate the num of people 
+        int numOfpeople = calPeople(requiredProbability);
+
+        // printing the probability of people needed for at least 69%
+        System.out.println ("Number of people needed for a probability of " + requiredProbability + ": " + numOfpeople);
+
+     }
 }
